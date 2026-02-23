5379add70806ad55aabd4f270e03dbfcc3902239
Linus Kuehne <kuehne.linus@gmail.com>
2026-02-23 16:25:29 +0100

    linear IRM

--- scripts/causal_chambers/baselines/__init__.py
+++ scripts/causal_chambers/baselines/__init__.py
@@ -5,10 +5,12 @@ not part of the main package => exist only for benchmarking
 """
 
 from .icp import ICPglmClassifier, ICPrfClassifier
+from .irm_2 import IRM2Classifier
 from .neural_net import ERMClassifier, IRMClassifier, VRExClassifier
 
 __all__ = [
     "IRMClassifier",
+    "IRM2Classifier",
     "VRExClassifier",
     "ERMClassifier",
     "ICPglmClassifier",
